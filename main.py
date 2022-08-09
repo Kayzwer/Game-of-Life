31d0e2d590a8de83dc4dba604b43d8283cd91d7b
Kayzwer Lee <lkyu753@gmail.com>
2022-08-09 22:56:42 +0800

    switch to np.bool8 data type

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ class World:
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.world = np.zeros((height, width), dtype=np.int0)
+        self.world = np.zeros((height, width), dtype=np.bool8)
+
+    def _get_world(self, i: int, j: int) -> np.int0:
+        return np.int0(self.world[i][j])
 
     def __str__(self) -> str:
         output = ""
         for row in self.world:
             for grid in row:
-                output += " " if grid == 0 else "0"
+                output += "0" if grid else " "
             output += "\n"
         return output
 
@@ -22,46 +25,61 @@ class World:
         for i in range(self.height):
             for j in range(self.width):
                 if i == 0 and j == 0:
-                    count = self.world[i + 1][j] + self.world[i][j + 1] + \
-                        self.world[i + 1][j + 1]
+                    count = self._get_world(i + 1, j) + \
+                        self._get_world(i, j + 1) + \
+                        self._get_world(i + 1, j + 1)
                 elif i == 0 and j == self.width - 1:
-                    count = self.world[i][j - 1] + self.world[i + 1][j - 1] + \
-                        self.world[i + 1][j]
+                    count = self._get_world(i, j - 1) + \
+                        self._get_world(i + 1, j - 1) + \
+                        self._get_world(i + 1, j)
                 elif i == self.height - 1 and j == 0:
-                    count = self.world[i - 1][j] + self.world[i - 1][j + 1] + \
-                        self.world[i][j + 1]
+                    count = self._get_world(i - 1, j) + \
+                        self._get_world(i - 1, j + 1) + \
+                        self._get_world(i, j + 1)
                 elif i == self.height - 1 and j == self.width - 1:
-                    count = self.world[i][j - 1] + self.world[i - 1][j - 1] + \
-                        self.world[i - 1][j]
+                    count = self._get_world(i, j - 1) + \
+                        self._get_world(i - 1, j - 1) + \
+                        self._get_world(i - 1, j)
                 elif i == self.height - 1:
-                    count = self.world[i][j - 1] + self.world[i - 1][j - 1] + \
-                        self.world[i - 1][j] + self.world[i - 1][j + 1] + \
-                        self.world[i][j + 1]
+                    count = self._get_world(i, j - 1) + \
+                        self._get_world(i - 1, j - 1) + \
+                        self._get_world(i - 1, j) + \
+                        self._get_world(i - 1, j + 1) + \
+                        self._get_world(i, j + 1)
                 elif j == self.width - 1:
-                    count = self.world[i - 1][j] + self.world[i - 1][j - 1] + \
-                        self.world[i][j - 1] + self.world[i + 1][j - 1] + \
-                        self.world[i + 1][j]
+                    count = self._get_world(i - 1, j) + \
+                        self._get_world(i - 1, j - 1) + \
+                        self._get_world(i, j - 1) + \
+                        self._get_world(i + 1, j - 1) + \
+                        self._get_world(i + 1, j)
                 elif i == 0:
-                    count = self.world[i][j - 1] + self.world[i + 1][j - 1] + \
-                        self.world[i + 1][j] + self.world[i + 1][j + 1] + \
-                        self.world[i][j + 1]
+                    count = self._get_world(i, j - 1) + \
+                        self._get_world(i + 1, j - 1) + \
+                        self._get_world(i + 1, j) + \
+                        self._get_world(i + 1, j + 1) + \
+                        self._get_world(i, j + 1)
                 elif j == 0:
-                    count = self.world[i - 1][j] + self.world[i - 1][j + 1] + \
-                        self.world[i][j + 1] + self.world[i + 1][j + 1] + \
-                        self.world[i + 1][j]
+                    count = self._get_world(i - 1, j) + \
+                        self._get_world(i - 1, j + 1) + \
+                        self._get_world(i, j + 1) + \
+                        self._get_world(i + 1, j + 1) + \
+                        self._get_world(i + 1, j)
                 else:
-                    count = self.world[i][j - 1] + self.world[i - 1][j - 1] + \
-                        self.world[i - 1][j] + self.world[i - 1][j + 1] + \
-                        self.world[i][j + 1] + self.world[i + 1][j + 1] + \
-                        self.world[i + 1][j] + self.world[i + 1][j - 1]
-
-                if new_world[i][j] == 1:
+                    count = self._get_world(i, j - 1) + \
+                        self._get_world(i - 1, j - 1) + \
+                        self._get_world(i - 1, j) + \
+                        self._get_world(i - 1, j + 1) + \
+                        self._get_world(i, j + 1) + \
+                        self._get_world(i + 1, j + 1) + \
+                        self._get_world(i + 1, j) + \
+                        self._get_world(i + 1, j - 1)
+                if new_world[i][j]:
                     if count < 2:
-                        new_world[i][j] = 0
+                        new_world[i][j] = False
                     elif count > 3:
-                        new_world[i][j] = 0
-                elif new_world[i][j] == 0 and count == 3:
-                    new_world[i][j] = 1
+                        new_world[i][j] = False
+                elif not new_world[i][j] and count == 3:
+                    new_world[i][j] = True
 
         self.world = new_world
 
@@ -71,10 +89,10 @@ class World:
         while i < number:
             row = np.random.randint(0, self.height)
             col = np.random.randint(0, self.width)
-            while self.world[row][col] == 1:
+            while self.world[row][col]:
                 row = np.random.randint(0, self.height)
                 col = np.random.randint(0, self.width)
-            self.world[row][col] = 1
+            self.world[row][col] = True
             i += 1
 
     def run(self) -> None:
